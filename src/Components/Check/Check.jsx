73d7c4d22f6b6febb044193c3166b1d678dd9098
jjulissa <julissajoseph9@gmail.com>
2023-02-21 17:59:30 +0100

    Tailwind

--- src/Components/Check/Check.jsx
+++ src/Components/Check/Check.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import Bizum from '../../assets/Bizum.png' 
+import Button from '../Button/Button.jsx'
 
 
 export default function Check() {
@@ -7,86 +9,93 @@ export default function Check() {
     return ( 
         <> 
             <main>
-                <section> 
-                    <h1>Checkout</h1>
-                    <article>
+                <section className="flex flex-col gap-6 flex-wrap p-10"> 
+                    <h1 className="text-center text-2xl font-medium text-verde">Checkout</h1>
+                    <article className="flex gap-6 p-2">
                         <div>
-                            <p>Seleccionar método de pago</p>
-                            <article> 
-                                <input type="radio" />
-                                <div> 
-                                    <p>Tarjeta de débito</p>
-                                    <p>Opción estándar sin seguimiento</p>
+                            <p className="text-lg p-2 font-semibold">Seleccionar método de pago</p>
+                            <article className="flex gap-4 p-2"> 
+                                <input type="radio" className="accent-green-900"/>
+                                <div className="flex flex-col pr-10 gap-1"> 
+                                    <p className="text-black text-xs font-semibold">Tarjeta de débito</p>
+                                    <p className="text-xs font-extralight">Opción estándar sin seguimiento</p>
                                 </div>
-                                <p>GRATIS</p>
+                                <p className="blur-2xl">GRATIS</p>
                             </article>
 
-                            <article>
-                                <div>
+                            <article className="flex flex-col gap-2 p-2 pb-5">
+                                <div className="flex flex-col gap-1 text-black text-xs font-normal">
                                     <label for="titular">Titular</label> 
-                                    <input type="text" placeholder="Nombre del titular" required/>
+                                    <input className="border w-72 rounded border-input" type="text" placeholder="Nombre del titular" required/>
                                 </div>
 
-                                <div>
+                                <div className="flex flex-col gap-1 text-black text-xs font-normal">
                                     <label>Número de la tarjeta</label> 
-                                    <input type="text"  placeholder="1234 1234 1234 1234" required/>
+                                    <input type="text" className="w-72 border rounded border-input" placeholder="1234 1234 1234 1234" required/>
                                 </div>
 
-                                <div>
-                                    <section>
+                                <div className="flex gap-2 text-black text-xs font-normal">
+                                    <section className="flex flex-col">
                                         <label for="FechaCaducidad">Fecha de caducidad</label> 
-                                        <input type="text" required/>
+                                        <input type="text" placeholder=" MM / YY" className="border rounded border-input" required/>
                                     </section>
 
-                                    <section>
+                                    <section className="flex flex-col">
                                         <label for="CVC">CVC</label> 
-                                        <input type="number" placeholder="123" required />
+                                        <input type="number" className="border rounded border-input" placeholder="123" required />
                                     </section>
                                 </div>
                             </article>
 
                             <hr />
 
-                            <article>
+                            <article className="flex gap-4 p-2 pt-5"> 
+                                <input type="radio" />
                                 <div> 
-                                    <p>Transferencia bancaria a la cuenta ES12 1234 1234 123457890</p>
-                                    <p>Será necesario recibir el comprobante de la transferencia para preparar tu pedido</p>
+                                    <p className="text-black text-xs font-semibold">Transferencia bancaria a la cuenta ES12 1234 1234 123457890</p>
+                                    <p className="text-xs font-extralight">Será necesario recibir el comprobante de la transferencia para preparar tu pedido</p>
                                 </div>
-                                    <p>9,00 €</p>
+                                    <p className="blur-2xl">9,00 €</p>
                             </article>
 
                             <div></div>
 
-                            <article>
-                                <div>
-                                    <p>Bizum</p>
+                            <article className="flex gap-4 p-2">
+                                <div className="flex text-sm items-center gap-4 font-semibold"> 
+                                    <input type="radio" />
+                                    <p>Bizum</p> 
+                                    <img className="w-14 h-6" src={Bizum} alt="Bizum" />
                                 </div>
                                 <p>9,00 €</p>
                             </article>
 
-                            <p>Dirección de envío</p>
+                            <p className="font-semibold text-base p-2.5">Dirección de envío</p>
 
-                            <article>
+                            <article className="flex flex-col gap-2 pl-2">
 
-                                <div>
-                                    <label for="name">Nombre</label>
+                                <div className="flex flex-col gap-1 text-xs font-normal text-black">
+                                    <label for="name">Nombre</label> 
+                                    <input className="border rounded border-input" type="text" placeholder="Nombre" required/>
                                 </div>
 
-                                <div>
-                                    <label for="apellidos">Apellidos</label>
+                                <div className="flex flex-col gap-1 text-xs font-normal text-black">
+                                    <label for="apellidos">Apellidos</label> 
+                                    <input className="border rounded border-input" type="text" placeholder="Apellido" required/>
                                 </div>
 
-                                <div>
-                                    <label for="name">Teléfono</label>
+                                <div className="flex flex-col gap-1 text-xs font-normal text-black">
+                                    <label for="name">Teléfono</label> 
+                                    <input className="border rounded border-input" type="tel" placeholder="+34 600 6000 600" required/>
                                 </div>
 
-                                <div>
-                                    <label for="name">Email</label>
+                                <div className="flex flex-col gap-1 text-xs font-normal text-black">
+                                    <label for="name">Email</label> 
+                                    <input className="border rounded border-input" type="email" placeholder="Email" required/>
                                 </div>
 
-                                <div>
+                                <div className="flex flex-col gap-1 text-xs font-normal text-black">
                                     <label for="name">País</label>
-                                    <select name="select" id="país" required> 
+                                    <select name="select" required> 
                                         <option value="seleccionar" selected>seleccionar</option>
                                         <option value="haiti">Haiti</option>
                                         <option value="espana">Espana</option>
@@ -94,31 +103,37 @@ export default function Check() {
                                     </select>
                                 </div>
 
-                                <div>
-                                    <article>
-                                        <label for="población">Población</label>
+                                <div className="flex gap-6 text-black text-xs font-normal">
+                                    <article className="flex flex-col gap-1">
+                                        <label>Población</label> 
+                                        <input className="border rounded border-input" type="text" placeholder="Población" required/>
                                     </article>
 
-                                    <article>
-                                        <label for="cp">CP</label>
+                                    <article className="flex flex-col gap-1">
+                                        <label >CP</label> 
+                                        <input className="border rounded border-input" type="text" placeholder="123" required/>
                                     </article>
                                 </div>
 
-                                <div>
-                                    <article>
-                                        <label for="calle">Calle</label>
+                                <div className="flex gap-3">
+                                    <article className="flex flex-col justify-start items-start">
+                                        <label>Calle</label> 
+                                        <input className="border rounded border-input" type="text" required/>
                                     </article>
 
-                                    <article>
-                                        <label for="nº">Nº</label>
+                                    <article className="flex flex-col justify-start items-start">
+                                        <label for="nº">Nº</label> 
+                                        <input className="border rounded border-input" type="text" required/>
                                     </article>
 
-                                    <article>
-                                        <label for="piso">Piso</label>
+                                    <article className="flex flex-col justify-start items-start">
+                                        <label for="piso">Piso</label> 
+                                        <input className="border rounded border-input" type="text" required/>
                                     </article>
                                     
-                                    <article>
-                                        <label for="puerta">Puerta</label>
+                                    <article className="flex flex-col justify-start items-start">
+                                        <label for="puerta">Puerta</label> 
+                                        <input className="hover:border-gris focus:border-verde border rounded border-input" type="text" required/>
                                     </article>
                                 </div>
                             </article>
@@ -126,36 +141,42 @@ export default function Check() {
                         </div>
 
 
-                        <div>
-                            <arcticle>
-                                <p>Total del carrito</p>
+                        <div className="bg-hover p-6 flex flex-col gap-4 self-start">
+                        <arcticle className="flex flex-col gap-3">
+                            <p className="text-black text-lg font-semibold">Total del carrito</p>
+                            <hr />
 
-                                <div ></div>
+                            <div className="flex gap-4 font-normal text-black justify-between text-sm">
+                                <p>SUBTOTAL</p>
+                                <p>0 €</p>
+                            </div>
 
-                                <div>
-                                    <p>SUBTOTAL</p>
-                                    <p>0 €</p>
+                            <div className="flex gap-4 font-normal text-black justify-between text-sm">
+                                <p>ENVÍO</p>
+                                <p >GRATIS</p>
+                            </div>
+
+                            <hr />
+
+                            <div className="flex justify-between gap-4">
+                                <p className="font-semibold text-black text-sm">TOTAL</p>
+                                <div className="flex flex-col gap-2">
+                                    <p className="font-semibold text-black text-sm text-right">0 €</p>
+                                    <p className="text-right text-xs text-gris font-normal">Incluye 3,78€ de IVA</p>
                                 </div>
+                            </div>
+                        </arcticle>
 
-                                <div>
-                                    <p>ENVÍO</p>
-                                    <p>GRATIS</p>
-                                </div>
-
-                                <div></div>
-
-                                <div>
-                                    <p>TOTAL</p>
-                                    <div>
-                                        <p>0 €</p>
-                                        <p>Incluye 3,78€ de IVA</p>
-                                    </div>
-                                </div>
-                            </arcticle>
-                            <arcticle>
-                                <Link to='/Succes'>Pagar y realizar pedido</Link>
-                            </arcticle> 
-                        </div>
+                        <arcticle className="flex gap-4" >
+                            <Link to="/Success"> 
+                                <Button 
+                                    TextButton="Pagar y realizar pedido" 
+                                    bgColor="bg-verde"
+                                    textColor='text-white'
+                                />
+                            </Link> 
+                        </arcticle>
+                    </div>
                     </article>
                 </section>
             </main>
--- src/Components/SuccesMain/SuccesMain.jsx
+++ src/Components/SuccesMain/SuccesMain.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import Button from "../Button/Button"; 
+import imgSucces from '../../assets/iconSucces.png'
 
 
 export default function SuccesMain() {
@@ -5,7 +8,63 @@ export default function SuccesMain() {
 
     return ( 
         <> 
-        
+            <main className="flex flex-col justify-center items-center gap-6 pb-5">
+                <section className="flex flex-col justify-center items-center gap-6"> 
+                    <picture className="flex justify-center p-4">
+                        <img src={imgSucces} alt="" />
+                    </picture>
+
+                    <p className="text-verde text-center font-medium text-2xl">El pedido ha sido realizado con éxito</p>
+                    <p className="flex w-96 text-center self-center justify-center items-center text-base font-normal">
+                        El pedido #12387 se encuentra en preparación. 
+                        Lo recibirás dentro de las fechas acordadas en tu envío. 
+                        Hemos enviado un ticket a tu correo electrónico.
+                    </p>
+                </section>
+
+                <article>                     
+                    <section className="flex p-6 flex-col self-center gap-6 bg-hover rounded ">
+                        <h2 className="text-lg font-semibold text-black">Tu pedido</h2>
+                        <hr /> 
+
+                        <div className="flex flex-col gap-4"> 
+                            <hr />
+
+                            <article className="flex text-sm font-semibold justify-between gap-4 text-black">
+                                <p>SUBTOTAL</p>
+                                <p>0 €</p>
+                            </article>
+
+                            <article className="flex text-sm font-semibold justify-between gap-4 text-black">
+                                <p>ENVÍO</p>
+                                <p>GRATIS</p>
+                            </article>
+
+                            <div></div>
+
+                            <article className="flex text-sm font-semibold justify-between gap-4 text-black">
+                                <p>TOTAL</p>
+                                <div>
+                                    <p>0 €</p>
+                                    <p>Incluye 3,78€ de IVA</p>
+                                </div>
+                            </article>
+                        </div>
+                    </section>
+                </article>
+
+                <section> 
+                    <button> 
+                        <Link to='/Tienda'> 
+                            <Button 
+                                bgColor="bg-verde"
+                                textColor='text-white'
+                                TextButton="Volver a la tienda"
+                            />
+                        </Link> 
+                    </button>
+                </section>
+            </main> 
         </>
     )
 }
